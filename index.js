984345c29c3cc4420e78e4c304b5b90799d2d9e8
Michael Matich <matich.mike@gmail.com>
2020-12-21 10:09:28 -0500

    audio playing added

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-document.querySelectorAll(".drum").forEach((button) => button.addEventListener("click", () => alert("hey")));
-
-
+document.querySelectorAll(".drum")
+  .forEach(
+    (button) => button.addEventListener("click", function() {
+        new Audio('sounds/tom-1.mp3').play();
+        this.style.color = "white";
+      }
+    )
+  );
