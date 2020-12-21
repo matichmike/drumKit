9c914f2b9d9716f75dc9fea5a3c3aff0ec8d64bd
Michael Matich <matich.mike@gmail.com>
2020-12-21 10:20:55 -0500

    switch statement for diff sounds

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 document.querySelectorAll(".drum")
   .forEach(
     (button) => button.addEventListener("click", function() {
-        new Audio('sounds/tom-1.mp3').play();
-        this.style.color = "white";
+        
+        let buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+          case "w":
+            new Audio('sounds/tom-1.mp3').play();
+            break;
+            case "a":
+            new Audio('sounds/tom-2.mp3').play();
+            break;
+            case "s":
+            new Audio('sounds/tom-3.mp3').play();
+            break;
+            case "d":
+            new Audio('sounds/tom-4.mp3').play();
+            break;
+            case "j":
+            new Audio('sounds/snare.mp3').play();
+            break;
+            case "k":
+            new Audio('sounds/crash.mp3').play();
+            break;
+            case "l":
+            new Audio('sounds/kick-bass.mp3').play();
+            break;
+        
+          default:
+            break;
+        }
       }
     )
   );
+
